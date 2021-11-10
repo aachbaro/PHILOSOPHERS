360377f3dcba7877cef6136211b465600a4dcc84
Adam ACHBAROU <aachbaro@e1r4p22.42.fr>
2021-11-10 15:13:30 +0100

    clean shit

--- TESTING/SRCS/main.c
+++
@@ -1,7 +0,0 @@
-#include "../thread.h"
-
-int	main(int ac, char **av)
-{
-	printf("%d\n", rand_int(5));
-	return (0);
-}
--- TESTING/SRCS/utils.c
+++
@@ -1,9 +0,0 @@
-#include "../thread.h"
-
-int	rand_int(int max)
-{
-	struct timeval time;
-
-	gettimeofday(&time, NULL);
-	return (time.tv_usec % max + 1);
-}
